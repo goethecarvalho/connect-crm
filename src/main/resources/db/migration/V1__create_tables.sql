478d386b0a7e762bb0577d8bff557b1cac1c0b2e
goethecarvalho <goethecarvalho@gmail.com>
2024-05-24 15:45:10 -0300

    Ajuste nos scripts de criação das tabelas

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -9,29 +9,24 @@ CREATE TABLE entidades (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     tipo VARCHAR(50) NOT NULL -- pode ser 'cliente', 'fornecedor' ou 'prestador'
-    -- outros campos específicos
+
     CONSTRAINT chk_tipo CHECK (tipo IN ('CLIENTE', 'FORNECEDOR', 'PRESTADOR'))
 );
 
 CREATE TABLE propostas (
     id SERIAL PRIMARY KEY,
     descricao VARCHAR(255) NOT NULL,
-    tipo VARCHAR(50) NOT NULL, -- pode ser 'cliente', 'fornecedor' ou 'prestador'
     data TIMESTAMP not null,
     valor DECIMAL(10, 2),
-    entidade_id bigint not null
-    -- outros campos específicos
-    CONSTRAINT chk_tipo CHECK (tipo IN ('cliente', 'fornecedor', 'prestador')),
+    entidade_id bigint not null,
     FOREIGN KEY (entidade_id) REFERENCES entidades (id)
 );
 
 CREATE TABLE transacoes (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    tipo VARCHAR(50) NOT NULL, -- pode ser 'cliente', 'fornecedor' ou 'prestador'
     data TIMESTAMP not null,
     valor DECIMAL(10, 2),
-    entidade_id bigint not null
-    -- outros campos específicos
-    CONSTRAINT chk_tipo CHECK (tipo IN ('cliente', 'fornecedor', 'prestador'))
+    entidade_id bigint not null,
+    FOREIGN KEY (entidade_id) REFERENCES entidades (id)
 );
