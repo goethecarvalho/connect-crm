9c31dc73c0edf6c4c019865f2ac5ffc1bf9d1a18
goethecarvalho <goethecarvalho@gmail.com>
2024-05-29 15:45:46 -0300

    Configuração do Cors para conexão com o frontend

--- src/main/java/br/com/connect/crm/controller/LoginController.java
+++ src/main/java/br/com/connect/crm/controller/LoginController.java
@@ -1,8 +1,6 @@
 package br.com.connect.crm.controller;
 
 import br.com.connect.crm.domain.usuario.vo.DadosLogin;
-import br.com.connect.crm.domain.usuario.vo.DadosUsuario;
-import br.com.connect.crm.domain.usuario.vo.LoginRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,14 +9,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api")
 public class LoginController {
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
-        if ("admin".equals(loginRequest.getUsername()) && "admin".equals(loginRequest.getPassword())) {
+    @Transactional
+    public ResponseEntity<String> login(@RequestBody @Valid DadosLogin dados, UriComponentsBuilder uriBuilder) {
+        if ("admin".equals(dados.getUsername()) && "admin".equals(dados.getPassword())) {
             return ResponseEntity.ok("Login successful");
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
--- src/main/java/br/com/connect/crm/controller/UsuarioController.java
+++ src/main/java/br/com/connect/crm/controller/UsuarioController.java
@@ -2,14 +2,12 @@ package br.com.connect.crm.controller;
 
 import br.com.connect.crm.domain.usuario.service.UsuarioService;
 import br.com.connect.crm.domain.usuario.vo.DadosDetalheUsuario;
-import br.com.connect.crm.domain.usuario.vo.DadosLogin;
 import br.com.connect.crm.domain.usuario.vo.DadosUsuario;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
