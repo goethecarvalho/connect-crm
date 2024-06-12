11654d20ff43167d0510fcd9845ee3811229d33f
goethecarvalho <goethecarvalho@gmail.com>
2024-06-12 13:41:45 -0300

    Implementação da consulta findAll na classe PropostaRepository

--- src/main/java/br/com/connect/crm/domain/proposta/repository/PropostaRepository.java
+++ src/main/java/br/com/connect/crm/domain/proposta/repository/PropostaRepository.java
@@ -1,14 +1,15 @@
 package br.com.connect.crm.domain.proposta.repository;
 
 import br.com.connect.crm.domain.proposta.entity.Proposta;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PropostaRepository extends JpaRepository<Proposta, Long> {
 
-    /*@Query("""
-        select p.id, p.numero, p.descricao, p.data, p.valor, p.status, e.nome, e.tipo
-        from propostas p inner join entidades e on e.id = p.entidade_id
-            """)
-    Page<DadosDetalheProposta> findAllPropostas(Pageable paginacao);*/
+    @Query("SELECT p FROM Proposta p JOIN FETCH p.entidade")
+    Page<Proposta> findAll(Pageable paginacao);
+
 
 }
